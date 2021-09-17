5830e65f1f9907bfa942ac65bca8d87c721a0742
Wallace Tácio <wallacett.dev@gmail.com>
2021-09-16 23:02:57 -0300

    Adiciona resolição do exercício Projetil ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Projetil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double pi = Math.PI;
+
+            double g = 9.80665;
+
+            string veloc, ang;
+
+            Console.WriteLine(" ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("-*-Projetil-*-");
+            Console.ResetColor();
+            Console.WriteLine(" ");
+
+            Console.Write("Entre com a velocidade, em m/s..: ");
+            veloc = Console.ReadLine();
+
+            Console.Write("Entre com o ângulo, em graus....: ");
+            ang = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            double v = Convert.ToDouble(veloc);
+            double a = Convert.ToDouble(ang);
+
+            double rad = (a * pi) / 180;
+
+            double senA = Math.Sin(rad);
+
+            double sen2A = Math.Sin((2*rad));
+
+            double xMax = Math.Pow(v,2) * (sen2A) / g;
+
+            double hMax = Math.Pow((v * senA), 2) / (2 * g);
+
+            Console.WriteLine($"Alcance........: {xMax:N2}m");
+            Console.WriteLine($"Altura máxima..: {hMax:N2}m");
+           Console.WriteLine(" ");
+
+
+        }
+    }
+}
